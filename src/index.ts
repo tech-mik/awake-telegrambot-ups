d50ec23754bc0c2f0c401be2025cc105c4d88079
Mik ten Holt <miktenholt@gmail.com>
2025-03-15 21:08:24 +0100

    Update webhook message to include additional text

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.post('/webhook', (req, res) => {
     const secret = authorization.split('Bearer ')[1]
     if (!secret || secret !== process.env.WEBHOOK_SECRET) return res.sendStatus(403)
 
-    bot.sendMessage(1483691115, 'Hello Mikkie Likkie Sikkie')
+    bot.sendMessage(1483691115, 'Hello Mikkie Likkie Sikkie Gikkie')
 
     res.sendStatus(200)
 })
