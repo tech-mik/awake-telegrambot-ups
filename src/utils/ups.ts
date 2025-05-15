be0b9e084438a4824e1bf1b046185a7fbe629fbf
Mik ten Holt <miktenholt@gmail.com>
2025-05-15 22:41:00 +0200

    test

--- src/utils/ups.ts
+++ src/utils/ups.ts
@@ -1,5 +1,12 @@
+import AppState from '../lib/state'
 import { UpsTelegramMessage, upsEventTypeTitle } from '../types/ups'
 
 export const generateTelegramMessage: UpsTelegramMessage = (level, upsName, message, timestamp) => {
-    return `${upsEventTypeTitle[level]}\n` + `${upsName}\n` + `${message}\n` + `${timestamp}`
+    const upsLocation = AppState.upsList.get(upsName)?.location || ''
+    return (
+        `${upsEventTypeTitle[level]}\n` +
+        `*${upsName} - ${upsLocation}*\n` +
+        `${message}\n` +
+        `${new Date(timestamp).toLocaleDateString()}`
+    )
 }
